4bc3c29cabe76027187a594a2e7a9b153fab7575
m'pkamou Samo <baobab@mpkamous-MacBook-Pro.local>
2026-02-11 15:10:56 +0100

    Add Tailwind CSS + create React components

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2D5016',
+        secondary: '#F5E6D3',
+      },
+    },
+  },
+  plugins: [],
+}
