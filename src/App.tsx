dffacff7df59cb3c462574e375b376217085c861
m'pkamou Samo <baobab@mpkamous-MacBook-Pro.local>
2026-02-11 15:18:04 +0100

    Test Tailwind - Add green button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div>
       <h1>Baobab</h1>
+      <button className="bg-primary text-white px-6 py-3 rounded-lg hover:bg-green-700">
+        Commencer gratuitement
+      </button>
     </div>
   )
 }
